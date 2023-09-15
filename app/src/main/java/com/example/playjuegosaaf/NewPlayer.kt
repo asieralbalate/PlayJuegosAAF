fce497c9875a43f347e85778449f2f91cf763c2c
asier <alualb1634@ieselcaminas.org>
2023-09-15 04:18:25 +0200

    First H Commit

--- app/src/main/java/com/example/playjuegosaaf/NewPlayer.kt
+++ app/src/main/java/com/example/playjuegosaaf/NewPlayer.kt
@@ -4,11 +4,17 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -16,6 +22,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,28 +31,116 @@ import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewPlayer(){
-    var pesoH = 1f
-    var pesoV = 2f
+fun NewPlayer() {
+    var pesoH1 = 2f
+    var pesoH2 = 3f
+    var pesoV1 = 1f
+    var pesoV2 = 2f
     val configuration = LocalConfiguration.current
     when (configuration.orientation) {
         Configuration.ORIENTATION_LANDSCAPE -> {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
             ) {
-                Row {
-
+                Spacer(modifier = Modifier.size(15.dp))
+                Row(
+                    Modifier
+                        .height(60.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.account),
+                        contentDescription = "AccountImage",
+                        Modifier
+                            .weight(pesoH1)
+                            .fillMaxSize()
+                    )
+                    TextField(
+                        value = "Nombre",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoH2)
+                            .padding(end = 10.dp)
+                    )
                 }
-                Spacer(modifier = Modifier.size(50.dp))
-                Spacer(modifier = Modifier.size(50.dp))
-                Spacer(modifier = Modifier.size(50.dp))
+                Spacer(modifier = Modifier.size(15.dp))
                 Row {
-
+                    Spacer(modifier = Modifier.weight(pesoH1))
+                    TextField(
+                        value = "Apellidos",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoH2)
+                            .padding(end = 10.dp)
+                    )
                 }
+                Spacer(modifier = Modifier.size(15.dp))
                 Row {
+                    Spacer(modifier = Modifier.weight(pesoH1))
+                    TextField(
+                        value = "Nickname",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoH2)
+                            .padding(end = 10.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.account),
+                        contentDescription = "AccountImage",
+                        Modifier
+                            .weight(pesoH1)
+                            .wrapContentSize()
+                    )
+                    Button(onClick = { /*TODO*/ }) {
 
+                    }
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                Row(
+                    Modifier
+                        .height(60.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.camera),
+                        contentDescription = "CameraImage",
+                        Modifier
+                            .weight(pesoH1)
+                            .fillMaxSize()
+                    )
+                    TextField(
+                        value = "Telefono",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoH2)
+                            .padding(end = 10.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                Row(
+                    Modifier
+                        .height(60.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.email),
+                        contentDescription = "EmailImage",
+                        Modifier
+                            .weight(pesoH1)
+                            .fillMaxSize()
+                    )
+                    TextField(
+                        value = "Email",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoH2)
+                            .padding(end = 10.dp)
+                    )
                 }
             }
         }
@@ -53,52 +148,109 @@ fun NewPlayer(){
         else -> {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
             ) {
-                Row {
-                    Image(painter = painterResource(id = R.drawable.account), contentDescription = "AccountImage",
-                        Modifier.weight(pesoH))
-                    TextField(value = "Account", onValueChange = {}, Modifier.weight(pesoV))
+                Spacer(modifier = Modifier.size(15.dp))
+                Row(
+                    Modifier
+                        .height(60.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.account),
+                        contentDescription = "AccountImage",
+                        Modifier
+                            .weight(pesoV1)
+                            .fillMaxSize()
+                    )
+                    TextField(
+                        value = "Nombre",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoV2)
+                            .padding(end = 10.dp)
+                    )
                 }
-                Spacer(modifier = Modifier.size(50.dp))
-                Spacer(modifier = Modifier.size(50.dp))
-                Spacer(modifier = Modifier.size(50.dp))
+                Spacer(modifier = Modifier.size(15.dp))
                 Row {
-
+                    Spacer(modifier = Modifier.weight(pesoV1))
+                    TextField(
+                        value = "Apellidos",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoV2)
+                            .padding(end = 10.dp)
+                    )
                 }
+                Spacer(modifier = Modifier.size(15.dp))
                 Row {
+                    Spacer(modifier = Modifier.weight(pesoV1))
+                    TextField(
+                        value = "Nickname",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoV2)
+                            .padding(end = 10.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.account),
+                        contentDescription = "AccountImage",
+                        Modifier
+                            .weight(pesoV1)
+                            .wrapContentSize()
+                    )
+                    Button(onClick = { /*TODO*/ }) {
 
+                    }
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                Row(
+                    Modifier
+                        .height(60.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.camera),
+                        contentDescription = "CameraImage",
+                        Modifier
+                            .weight(pesoV1)
+                            .fillMaxSize()
+                    )
+                    TextField(
+                        value = "Telefono",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoV2)
+                            .padding(end = 10.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                Row(
+                    Modifier
+                        .height(60.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.email),
+                        contentDescription = "EmailImage",
+                        Modifier
+                            .weight(pesoV1)
+                            .fillMaxSize()
+                    )
+                    TextField(
+                        value = "Email",
+                        onValueChange = {},
+                        Modifier
+                            .weight(pesoV2)
+                            .padding(end = 10.dp)
+                    )
                 }
             }
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row {
 
-                }
-                Spacer(modifier = Modifier.size(50.dp))
-                Row {
-
-                }
-                Spacer(modifier = Modifier.size(50.dp))
-                Row {
-
-                }
-                Spacer(modifier = Modifier.size(50.dp))
-                Row {
-
-                }
-                Spacer(modifier = Modifier.size(50.dp))
-                Row {
-
-                }
-                Row {
-
-                }
-            }
         }
     }
 }
